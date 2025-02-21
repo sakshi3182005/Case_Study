7fd376a4120d3df98c5820a6b9cd06e83a5ad7ce
vikshitha shetty <vikshithashetty9@gmail.com>
2025-02-21 10:32:26 +0530

    Day5_vik.c

---
+++ Day5_vik.c
@@ -0,0 +1,8 @@
+void readStudent(Student std[],int n)
+{
+	printf("Enter the details(Rollno,Name,Maths marks,Physics marks,Chemistry marks,CS marks)";
+	for(int i=0; i<n; i++) 
+	{
+		scanf("%d%s%d%d%d%d",&std[i].rollno,std[i].name,&std[i].maths marks,std[i].physics marks,std[i].chemistry marks,std[i].cs marks);
+	}
+}// Reading the student details
